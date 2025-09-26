341e0c459fd6fe9f13a424eb2cf2a811aa0227ac
Blas <montanariblas@hotmail.com>
2025-09-26 09:19:14 -0300

    fix leaflet

---
+++ src/app/components/Contact/MapComponent.jsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import styles from "./Contact.module.css";
+
+const MapComponent = ({ position }) => {
+  useEffect(() => {
+    // Fix Leaflet marker icon issue in Next.js
+    delete L.Icon.Default.prototype._getIconUrl;
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl:
+        "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
+      iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+      shadowUrl:
+        "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
+    });
+  }, []);
+
+  return (
+    <MapContainer
+      center={position}
+      zoom={15}
+      scrollWheelZoom={false}
+      className={styles.map}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          <strong>Taller Suarez</strong>
+          <br />
+          C. 19 N 1794 Esq. 514
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default MapComponent;
